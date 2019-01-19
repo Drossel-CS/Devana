5c578081d8be8abc32123447d0d467a3ec9fd868
Peter Kriška <pekriska@gmail.com>
2019-01-19 21:28:49 +0100

    responsive

--- js/navigation.js
+++ js/navigation.js
@@ -30,19 +30,22 @@
 $ = jQuery.noConflict(true);
 
 $(document).ready( function () {
-$("#primary-menu").css("display","none");
+var breakpoint = 870;
+if ($(document).width() <= breakpoint){
+  $("#primary-menu").css("display","none");
 
-$('.menu-toggle').click(function(e) {
-   e.preventDefault();
-    if ($('#primary-menu').is(":visible") ) {
-      $('.menu-toggle').toggleClass("is-active");
-      $('#primary-menu').stop(true,true).slideUp(700);
-      
-    } else {
-      $('.menu-toggle').toggleClass("is-active");
-      $('#primary-menu').stop(true,true).slideDown(700);
-      
-    }
-  });
+  $('.menu-toggle').click(function(e) {
+     e.preventDefault();
+      if ($('#primary-menu').is(":visible") ) {
+        $('.menu-toggle').toggleClass("is-active");
+        $('#primary-menu').stop(true,true).slideUp(700);
+        
+      } else {
+        $('.menu-toggle').toggleClass("is-active");
+        $('#primary-menu').stop(true,true).slideDown(700);
+        
+      }
+    });
+}
 
 });
